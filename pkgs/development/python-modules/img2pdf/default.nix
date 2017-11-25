da3876225dca20be53e2cf12e8d2f3dd6989f044
sjau <github.com@sjau.ch>
2017-11-25 23:50:08 +0100

    Adding OCRmyPDF and new required python modules

---
+++ pkgs/development/python-modules/img2pdf/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, lib, buildPythonPackage, fetchPypi, isPyPy, python3, callPackage }:
+
+with python3.pkgs;
+
+buildPythonPackage rec {
+  pname = "img2pdf";
+  version = "0.2.4";
+  name = "${pname}-${version}";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1bn9zzsmg4k41wmx1ldmj8a4yfj807p8r0a757lm9yrv7bx702ql";
+  };
+
+  propagatedBuildInputs = [
+    pillow
+    pdfrw
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = https://gitlab.mister-muffin.de/josch/img2pdf;
+    description = "Losslessly convert raster images to PDF. ";
+    license = lib.licenses.lgpl;
+    maintainers = with lib.maintainers; [ hyper_ch ];
+  };
+}
