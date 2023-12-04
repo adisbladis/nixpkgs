60dcb772a1b85d5ad4d433c1446147f35d1b96be
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-04 03:13:01 +0100

    python311Packages.dbus-python: 1.2.18 -> 1.3.2

---
+++ pkgs/development/python-modules/dbus-python/default.nix
@@ -0,0 +1,83 @@
+{ lib
+, fetchPypi
+, buildPythonPackage
+, isPyPy
+
+# build-system
+, meson
+, meson-python
+, ninja
+, patchelf
+, pkg-config
+, setuptools
+
+# native dependencies
+, dbus
+, dbus-glib
+
+# tests
+, pygobject3
+}:
+
+buildPythonPackage rec {
+  pname = "dbus-python";
+  version = "1.3.2";
+  pyproject = true;
+
+  disabled = isPyPy;
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-rWeBkwhhi1BpU3viN/jmjKHH/Mle5KEh/mhFsUGCSPg=";
+  };
+
+  postPatch = ''
+    sed -i '/patchelf/d' pyproject.toml
+    rm configure.ac configure
+  '';
+
+  nativeBuildInputs = [
+    meson
+    meson-python
+    ninja
+    patchelf
+    pkg-config
+    setuptools
+  ];
+
+  buildInputs = [
+    dbus
+    dbus-glib
+  ];
+
+  nativeCheckInputs = [
+    dbus.out
+    pygobject3
+  ];
+
+  doCheck = false; # ERROR: No such build data file as '/build/dbus-python-1.3.2/meson-private/build.dat'.
+
+  checkPhase = ''
+    runHook preCheck
+
+    dbus-run-session \
+      --config-file=${dbus}/share/dbus-1/session.conf \
+        meson test \
+          --print-errorlogs
+
+    runHook postCheck
+  '';
+
+  meta = with lib; {
+    description = "Python DBus bindings";
+    homepage = "https://gitlab.freedesktop.org/dbus/dbus-python";
+    license = licenses.mit;
+    platforms = dbus.meta.platforms;
+    maintainers = with maintainers; [ ];
+  };
+}
